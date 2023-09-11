d18f8b826ce6bca277a4729f8c2e92bb2b06c06a
salamioluwatobi <salamitobimi@gmail.com>
2023-09-11 11:43:07 +0100

    okay

---
+++ README.old.md
@@ -0,0 +1,1 @@
+A simple landing page - first designed in figma and then coded in React. Hope you like it.
--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import "./App.css";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Work from "./Components/Work";
-import Testimonial from "./Components/Testimonial";
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
-      <Work />
-      <Testimonial />
-      <Contact />
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
